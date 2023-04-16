1cc8d261e2c58bda0632b9517bb02f2ed869fcb2
squidmin <morse.james.r@gmail.com>
2023-04-15 19:47:40 -0500

    Minor edits (cont'd)

--- src/test/java/org/squidmin/bigquery/config/IntegrationTestConfig.java
+++ src/test/java/org/squidmin/bigquery/config/IntegrationTestConfig.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.squidmin.bigquery.util.TableFactory;
 
 @Configuration
 @Profile("integration")
